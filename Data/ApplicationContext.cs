71a70d3bfe725f471fb84a715d6bf7cfd7fb74c5
Matheus Canovas <matheuscanovas9@gmail.com>
2024-12-19 11:01:23 -0300

    Criando classes de entidades

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using DominandoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DominandoEFCore.Data {
+    public class ApplicationContext : DbContext {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            const string strConnection = "Server=127.0.0.1; Port=5432; User ID=postgres; Password=Y7QXzeybI0VChLw; Database=curso-desenvolvedor-io; Pooling=true; MinPoolSize=0; MaxPoolSize=5; Connection Lifetime=3000; Timeout=15";
+            optionsBuilder
+                .UseNpgsql(strConnection)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+}
