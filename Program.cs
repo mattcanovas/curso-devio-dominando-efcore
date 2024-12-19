68fd780797f6c03f2079b783ca8c363fcafa4c81
Matheus Canovas <matheuscanovas9@gmail.com>
2024-12-19 11:06:25 -0300

    Entendendo EnsureCreated e EnsureDeleted
    Comandos que são utilizados para criar e deletar o banco de dados ao
    executar o programa em ambiente de desenvolvimento

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DominandoEFCore.Data;
+
+class Program {
+    private static void Main(string[] args) {
+        EnsureCreatedAndDeleted();
+    }
+
+    static void EnsureCreatedAndDeleted() {
+        using var db = new ApplicationContext();
+        //db.Database.EnsureCreated();
+        db.Database.EnsureDeleted();
+    }
+}
